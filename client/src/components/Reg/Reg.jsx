794e9703830cbcbbeb9999f5664660c3d5d9d937
Dima <schtanckevitch.dima@yandex.ru>
2022-08-09 11:43:11 +0300

    add easy form

--- client/src/components/Reg/Reg.jsx
+++ client/src/components/Reg/Reg.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import styles from './Reg.module.css'
 
 export default function Reg() {
   return (
-    <div>Reg</div>
+    <div>
+      <br />
+      <form className={styles.FormReg}>
+        <label>
+          name
+          <br />
+          <input type='name'/>
+        </label>
+        <br />
+        <label>
+          email
+          <br />
+          <input type='email'/>
+        </label>
+        <br />
+        <label>
+          password
+          <br />
+         <input type='password'/>
+        </label>
+        <br />
+        <button type='button'>send</button>
+      </form>
+    </div>
   )
 }
